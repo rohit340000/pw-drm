a4a9b8c105c5f713319a4c874328c878e032ceb3
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-07-10 21:55:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = API_ID =  28590119
+API_ID = API_ID =  25891183
 
-API_HASH = os.environ.get("API_HASH", "2494557bf21e6c5152f26070aa1a97c7")
+API_HASH = os.environ.get("API_HASH", "36709c81d7609a81f86de931cbc87f3a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5288671031:AAHUlujBnRaQbgsxszQzZn-voY8UIgSLCyg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6677611444:AAG1b6eR2LfUDhbqaiHdpRbslo3FHIbfpgY")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 1923922961))
+OWNER = int(os.environ.get("OWNER", 6200203086))
 
 LOG = -1001605524352,
 
-# UPDATE_GRP = -1002140332321, # bot sat group
+# UPDATE_GRP = -1002168262667, # bot sat group
 
-# auth_chats = [5219193259,1327415906]
+# auth_chats = [-1002235760775]
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1923922961").split()):
+    for x in (os.environ.get("ADMINS", "6200203086").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
